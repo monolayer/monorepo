03b5db33a8d6f744bf00138dfa821d3ee108f3d0
Marcos Essindi <marcessindi@me.com>
2024-02-01 15:29:57 +0100

    Export from indexes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "~/config.js";
 export * from "~/database/schema/columns.js";
 export * from "~/database/schema/table.js";
 export * from "~/database/schema/database.js";
+export * from "~/database/schema/indexes.js";
