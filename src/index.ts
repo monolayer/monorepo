565a42f7698eaa6cbd8e25a561125bfd29663fb3
Marcos Essindi <marcessindi@me.com>
2024-02-08 18:21:48 +0100

    Export constaints

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ export * from "~/database/schema/pg_column.js";
 export * from "~/database/schema/pg_table.js";
 export * from "~/database/schema/pg_database.js";
 export * from "~/database/schema/pg_index.js";
+export * from "~/database/schema/pg_foreign_key.js";
+export * from "~/database/schema/pg_primary_key.js";
+export * from "~/database/schema/pg_unique.js";
 export { pgPool } from "~/database/pg/pg_pool.js";
