0c92a1d0a81186774e10833704b82954ef321b60
Marcos Essindi <marcessindi@me.com>
2024-02-26 15:09:12 +0100

    Add pgBoolean Zod schema

--- src/database/schema/zod.ts
+++ src/database/schema/zod.ts
@@ -1,14 +1,46 @@
 import { z } from "zod";
-import { PgBoolean, PgColumnBase, PgColumnTypes } from "./pg_column.js";
+import {
+	PgBoolean,
+	PgColumnBase,
+	PgColumnTypes,
+	PgEnum,
+	PgText,
+} from "./pg_column.js";
 
 export function zodSchema(column: PgColumnTypes) {
 	switch (column.constructor) {
 		case PgBoolean:
 			isBoolean(column);
 			return booleanSchema(column);
+		case PgText:
+			isText(column);
+			return textSchema(column);
 		default:
 			return z.unknown();
 	}
+}
+
+function columnSchemaWithBase(
+	column: PgColumnTypes,
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	base: z.ZodType<any, any, any>,
+) {
+	if (column instanceof PgEnum) {
+		return base;
+	}
+	const info = PgColumnBase.info(column);
+
+	if (
+		(column._isPrimaryKey && info.defaultValue !== null) ||
+		(info.isNullable === false && info.defaultValue !== null)
+	) {
+		return base.optional();
+	}
+
+	if (!column._isPrimaryKey && info.isNullable === true) {
+		return base.nullable().optional();
+	}
+	return base;
 }
 
 function isBoolean(column: PgColumnTypes): asserts column is PgBoolean {
@@ -23,17 +55,18 @@ function booleanSchema(column: PgBoolean) {
 		.boolean()
 		.or(z.string().refine((s) => s === "true" || s === "false"))
 		.pipe(z.coerce.boolean());
-	const info = PgColumnBase.info(column);
 
-	if (
-		(column._isPrimaryKey && info.defaultValue !== null) ||
-		(info.isNullable === false && info.defaultValue !== null)
-	) {
-		return base.optional();
-	}
+	return columnSchemaWithBase(column, base);
+}
 
-	if (!column._isPrimaryKey && info.isNullable === true) {
-		return base.nullable().optional();
+function isText(column: PgColumnTypes): asserts column is PgText {
+	if (column instanceof PgText) {
+		return;
 	}
-	return base;
+	throw new Error("Only a PgText column is allowed");
+}
+
+function textSchema(column: PgText) {
+	const base = z.string();
+	return columnSchemaWithBase(column, base);
 }
--- tests/unit/zod.test.ts
+++ tests/unit/zod.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { boolean } from "~/database/schema/pg_column.js";
+import { boolean, text } from "~/database/schema/pg_column.js";
 import { zodSchema } from "~/database/schema/zod.js";
 
 describe("zod column schemas", () => {
@@ -84,6 +84,83 @@ describe("zod column schemas", () => {
 				column._isPrimaryKey = true;
 				const schema = zodSchema(column);
 				expect(schema.safeParse(true).success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(true);
+			});
+		});
+	});
+
+	describe("PgText", () => {
+		describe("by default", () => {
+			test("parses only strings, null and undefined", () => {
+				const column = text();
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(true);
+				expect(schema.safeParse(undefined).success).toBe(true);
+				expect(schema.safeParse(true).success).toBe(false);
+				expect(schema.safeParse(new Date()).success).toBe(false);
+				expect(schema.safeParse(1).success).toBe(false);
+			});
+
+			test("with default value is nullable and optional", () => {
+				const column = text().defaultTo("1");
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(true);
+				expect(schema.safeParse(undefined).success).toBe(true);
+			});
+
+			test("with notNull is non nullable and required", () => {
+				const column = text().notNull();
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(false);
+			});
+
+			test("with default and notNull is non nullable and optional", () => {
+				const column = text().notNull().defaultTo("1");
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(true);
+			});
+		});
+
+		describe("as primary key", () => {
+			test("is non nullable and required", () => {
+				const column = text();
+				column._isPrimaryKey = true;
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(false);
+			});
+
+			test("with default value is non nullable and optional", () => {
+				const column = text().defaultTo("hello");
+				column._isPrimaryKey = true;
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(true);
+			});
+
+			test("with notNull is non nullable and required", () => {
+				const column = text().notNull();
+				column._isPrimaryKey = true;
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
+				expect(schema.safeParse(null).success).toBe(false);
+				expect(schema.safeParse(undefined).success).toBe(false);
+			});
+
+			test("with default and notNull is non nullable and optional", () => {
+				const column = text().notNull().defaultTo("1");
+				column._isPrimaryKey = true;
+				const schema = zodSchema(column);
+				expect(schema.safeParse("hello").success).toBe(true);
 				expect(schema.safeParse(null).success).toBe(false);
 				expect(schema.safeParse(undefined).success).toBe(true);
 			});
