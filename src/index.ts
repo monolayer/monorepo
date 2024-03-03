574fcdcbd4992f99069f5d4e0b9a22b20823fb48
Marcos Essindi <marcessindi@me.com>
2024-03-03 18:23:53 +0100

    Export only constructors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
-export * from "~/database/schema/pg_column.js";
-export * from "~/database/schema/pg_table.js";
-export * from "~/database/schema/pg_database.js";
-export * from "~/database/schema/pg_index.js";
-export * from "~/database/schema/pg_foreign_key.js";
-export * from "~/database/schema/pg_unique.js";
-export * from "~/database/schema/pg_trigger.js";
+export { pgTable } from "~/database/schema/pg_table.js";
+export { pgDatabase } from "~/database/schema/pg_database.js";
+export { index } from "~/database/schema/pg_index.js";
+export { foreignKey } from "~/database/schema/pg_foreign_key.js";
+export { unique } from "~/database/schema/pg_unique.js";
+export { trigger } from "~/database/schema/pg_trigger.js";
 export * from "~/database/schema/pg_extension.js";
+export {
+	bigserial,
+	serial,
+	boolean,
+	text,
+	bigint,
+	bytea,
+	date,
+	doublePrecision,
+	float4,
+	float8,
+	int2,
+	int4,
+	int8,
+	integer,
+	json,
+	jsonb,
+	real,
+	uuid,
+	varchar,
+	char,
+	time,
+	timetz,
+	timestamp,
+	timestamptz,
+	numeric,
+	pgEnum,
+} from "~/database/schema/pg_column.js";
