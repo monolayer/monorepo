0840824b15f822b62f58a8e5355f576898d1b15b
Marcos Essindi <marcessindi@me.com>
2024-03-12 13:50:33 +0100

    Add infer doc

--- src/schema/pg_database.ts
+++ src/schema/pg_database.ts
@@ -17,6 +17,80 @@ export class PgDatabase<T extends ColumnRecord> {
 		};
 	}
 
+	/**
+	 * Infers the Kysely database schema type definition database with differents type for select, insert and update operations.
+	 *
+	 * The inference takes into account column default values and constraints (not null, primary key) to infer the appropriate type for the database operations.
+	 *
+	 * The type should be used as thedDatabase interface for the `Kysely` instance.
+	 *
+	 * @remarks
+	 * This property does not contain any value. Should be used only as a type with `typeof`.
+	 *
+	 * @example
+	 *
+	 * Given this database schema:
+	 *
+	 * ```ts
+	 * const database = pgDatabase({
+	 *   tables: {
+	 *     users: table({
+	 *       columns: {
+	 *         id: integer().generatedByDefaultAsIdentity(),
+	 *         name: varchar().notNull(),
+	 *       }
+	 *     }),
+	 *     books: table({
+	 *       id: integer().generatedByDefaultAsIdentity(),
+	 *       title: varchar(),
+	 *       copies: int2().notNull().default(0),
+	 *     }),
+	 *   }
+	 * });
+	 * ```
+	 *
+	 * Inferring the database type with:
+	 *
+	 * ```ts
+	 * type DB = typeof database.infer;
+	 * ```
+	 *
+	 * will result in the following type:
+	 *
+	 * ```ts
+	 * type DB = {
+	 *   users: {
+	 *     id: {
+	 *       readonly __select__: number;
+	 *       readonly __insert__: string | number | undefined;
+	 *       readonly __update__: string | number;
+	 *     };
+	 *     name: {
+	 *       readonly __select__: string;
+	 *       readonly __insert__: string;
+	 *       readonly __update__: string;
+	 *     };
+	 *   };
+	 *   books: {
+	 *     id: {
+	 *       readonly __select__: number;
+	 *       readonly __insert__: string | number | undefined;
+	 *       readonly __update__: string | number;
+	 *     };
+	 *     title: {
+	 *       readonly __select__: string | null;
+	 *       readonly __insert__: string | null | undefined;
+	 *       readonly __update__: string | null;
+	 *     };
+	 *     copies: {
+	 *       readonly __select__: number;
+	 *       readonly __insert__: string | number | undefined;
+	 *       readonly __update__: string | number;
+	 *     };
+	 *   };
+	 * };
+	 * ```
+	 */
 	declare infer: {
 		[K in keyof T]: T[K]["infer"];
 	};
