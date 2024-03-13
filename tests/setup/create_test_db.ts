911d5e0264abeb445e32709f73a9ccaff2ca4195
Marcos Essindi <marcessindi@me.com>
2024-03-13 18:27:08 +0100

    Test with multiple Kysely versions

--- tests/setup/create_test_db.ts
+++ tests/setup/create_test_db.ts
@@ -6,7 +6,11 @@ dotenv.config();
 const pool = new pg.Pool({ connectionString: env.POSTGRES_URL });
 
 pool.query(`DROP DATABASE IF EXISTS ${env.DATABASE_NAME}`).then(() => {
-	pool.query(`CREATE DATABASE ${env.DATABASE_NAME}`).then(() => {
+	pool.query(`CREATE DATABASE ${env.DATABASE_NAME}`).then(() => {});
+});
+
+pool.query(`DROP DATABASE IF EXISTS test_remote_schema`).then(() => {
+	pool.query(`CREATE DATABASE test_remote_schema`).then(() => {
 		pool.end().then(() => {});
 	});
 });
