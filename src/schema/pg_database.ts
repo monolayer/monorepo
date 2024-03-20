848367d69e3f0ae75ab422e74dae06b9c2505eb6
Marcos Essindi <marcessindi@me.com>
2024-03-20 16:58:02 +0100

    Document infer method

--- src/schema/pg_database.ts
+++ src/schema/pg_database.ts
@@ -22,75 +22,167 @@ export class PgDatabase<T extends ColumnRecord> {
 	}
 
 	/**
-	 * Infers the Kysely database schema type definition database with differents type for select, insert and update operations.
-	 *
-	 * The inference takes into account column default values and constraints (not null, primary key) to infer the appropriate type for the database operations.
-	 *
-	 * The type should be used as thedDatabase interface for the `Kysely` instance.
-	 *
+	 * Infers types to be used as the Kysely database schema type definition.
+	 * @public
 	 * @remarks
-	 * This property does not contain any value. Should be used only as a type with `typeof`.
 	 *
-	 * @example
+	 * Infers types for select, insert and update operations, taking into account the
+	 * column data type, constraints, generated values, and default data values.
 	 *
-	 * Given this database schema:
+	 * **Typescript Types**
 	 *
+	 * Each column has a Typescript type for its select, insert, and update operations:
+	 * | Column                | Select       | Insert                                                          | Update|
+	 * | :---                  | :----:       | :----:                                                          | :----:|
+	 * | bigint                | `string`     | &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;| &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;|
+	 * | bigserial             | `string`     | &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;| &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;|
+	 * | bit                   | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | bitVarying            | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | boolean               | `boolean`    | &#160;&#160;`boolean` &#124; `Boolish`*&#160;&#160;             | &#160;&#160;`boolean` &#124; `Boolish`*&#160;&#160;|
+	 * | bytea                 | `Buffer`     | &#160;&#160;`Buffer` &#124; `string`&#160;&#160;                | &#160;&#160;`Buffer` &#124; `string`&#160;&#160;|
+	 * | characterVarying      | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | character             | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | cidr                  | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | date                  | `Date`       | &#160;&#160;`Date` &#124; `string`&#160;&#160;                  | &#160;&#160;`Date` &#124; `string`&#160;&#160;|
+	 * | doublePrecision       | `string`     | &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;| &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;|
+	 * | enumerated            | enum values  | &#160;&#160;enum values&#160;&#160;                             | &#160;&#160;enum values&#160;&#160;|
+	 * | inet                  | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | integer               | `number`     | &#160;&#160;`number` &#124; `string`&#160;&#160;                | &#160;&#160;`number` &#124; `string`&#160;&#160;|
+	 * | json                  | `JsonValue`* | &#160;&#160;`JsonValue`*&#160;&#160;                            | &#160;&#160;`JsonValue`*&#160;&#160;|
+	 * | jsonb                 | `JsonValue`* | &#160;&#160;`JsonValue`*&#160;&#160;                            | &#160;&#160;`JsonValue`*&#160;&#160;|
+	 * | macaddr               | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | macaddr8              | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | numeric               | `string`     | &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;| &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;|
+	 * | real                  | `number`     | &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;| &#160;&#160;`bigint` &#124; `number` &#124; `string`&#160;&#160;|
+	 * | serial                | `number`     | &#160;&#160;`number` &#124; `string`&#160;&#160;                | &#160;&#160;`number` &#124; `string`&#160;&#160;|
+	 * | smallint              | `number`     | &#160;&#160;`number` &#124; `string`&#160;&#160;                | &#160;&#160;`number` &#124; `string`&#160;&#160;|
+	 * | time                  | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | timeWithTimeZone      | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | timestamp             | `Date`       | &#160;&#160;`Date` &#124; `string`&#160;&#160;                  | &#160;&#160;`Date` &#124; `string`&#160;&#160;|
+	 * | timestampWithTimeZone | `Date`       | &#160;&#160;`Date` &#124; `string`&#160;&#160;                  | &#160;&#160;`Date` &#124; `string`&#160;&#160;|
+	 * | tsquery               | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | tsvector              | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | uuid                  | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 * | xml                   | `string`     | &#160;&#160;`string`&#160;&#160;                                | &#160;&#160;`string`&#160;&#160;|
+	 *
+	 * (*) `Boolish` and `JsonValue` are defined as follows:
 	 * ```ts
+	 * type Boolish = "true" | "false" | "yes" | "no" | 1 | 0 | "1" | "0" | "on" | "off";
+	 * type JsonArray = JsonValue[];
+	 * type JsonValue = boolean | number | string | Record<string, unknown> | JsonArray;
+	 * ```
+	 *
+	 * **Optionality and nullability**
+	 *
+	 * By default, columns have optional inserts and updates:
+	 *
+	 * *Type of an `integer` named id*
+	 * ```ts
+	 * type id = {
+	 *   readonly __select__: number;
+	 *   readonly __insert__: number | string | undefined;
+	 *   readonly __update__: number | string;
+	 * };
+	 * ```
+	 * Columns that are generated always as identity will not accept inserts or updates:
+	 *
+	 * *Type of an `integer` column with generated always as identity named id*
+	 * ```ts
+	 * type id = {
+	 *   readonly __select__: number;
+	 *   readonly __insert__: never;
+	 *   readonly __update__: never;
+	 * };
+	 * ```
+	 *
+	 * Columns that either:
+	 * - have a primary key constraint
+	 * - have a `NOT NULL` constraint
+	 * - have a default data value
+	 * - are generated (`serial`, `bigserial`, or generated by default as identity)
+	 *
+	 * will have their optionality and nullability changed according to this table:
+	 * |             Column type              | &#160;&#160;Optional&#160;&#160;| &#160;&#160;Nullable&#160;&#160;|
+	 * | :---                                 | :----:      | :----:     |
+	 * | `NOT NULL` constraint                | no          | never      |
+	 * | primary key constraint               | no          | never      |
+	 * | with default data value              | always      | yes        |
+	 * | `serial`                             | always      | never      |
+	 * | `bigserial`                          | always      | never      |
+	 * | generated by default as identity     | always      | never      |
+	 *
+	 * *Type of an `integer` column with a `NOT NULL` constraint named id*
+	 * ```ts
+	 * type id = {
+	 *   readonly __select__: number;
+	 *   readonly __insert__: number | string;
+	 *   readonly __update__: number | string;
+	 * };
+	 * ```
+	 * *Type of an `integer` column with a default data value and a `NOT NULL` constraint named id*
+	 * ```ts
+	 * type id = {
+	 *   readonly __select__: number;
+	 *   readonly __insert__: number | string | undefined;
+	 *   readonly __update__: number | string;
+	 * };
+	 * ```
+	 * *Type of an `bigint` column generated by default as identity*
+	 * ```ts
+	 * type id = {
+	 *   readonly __select__: string;
+	 *   readonly __insert__: bigint | number | string | undefined;
+	 *   readonly __update__: bigint | number | string;
+	 * };
+	 * ```
+	 * @example
+	 * ```ts
+	 * const userRole = enumType("user_role", ["admin", "user"]);
 	 * const database = pgDatabase({
+	 *   types: [userRole],
 	 *   tables: {
 	 *     users: table({
 	 *       columns: {
-	 *         id: integer().generatedByDefaultAsIdentity(),
-	 *         name: varchar().notNull(),
-	 *       }
-	 *     }),
-	 *     books: table({
-	 *       id: integer().generatedByDefaultAsIdentity(),
-	 *       title: varchar(),
-	 *       copies: smallint().notNull().default(0),
+	 *         id: integer().generatedAlwaysAsIdentity(),
+	 *         name: text(),
+	 *         role: enumerated(userRole).notNull(),
+	 *         createdAt: timestampWithTimeZone().default(sql`now`).notNull(),
+	 *       },
+	 *       constraints: {
+	 *         primaryKey: primaryKey(["id"]),
+	 *       },
 	 *     }),
 	 *   }
 	 * });
-	 * ```
 	 *
-	 * Inferring the database type with:
-	 *
-	 * ```ts
 	 * type DB = typeof database.infer;
 	 * ```
 	 *
-	 * will result in the following type:
+	 * The `DB` type will be:
 	 *
 	 * ```ts
 	 * type DB = {
 	 *   users: {
 	 *     id: {
 	 *       readonly __select__: number;
-	 *       readonly __insert__: string | number | undefined;
-	 *       readonly __update__: string | number;
+	 *       readonly __insert__: never;
+	 *       readonly __update__: never;
 	 *     };
 	 *     name: {
 	 *       readonly __select__: string;
-	 *       readonly __insert__: string;
-	 *       readonly __update__: string;
-	 *     };
-	 *   };
-	 *   books: {
-	 *     id: {
-	 *       readonly __select__: number;
-	 *       readonly __insert__: string | number | undefined;
-	 *       readonly __update__: string | number;
-	 *     };
-	 *     title: {
-	 *       readonly __select__: string | null;
 	 *       readonly __insert__: string | null | undefined;
 	 *       readonly __update__: string | null;
-	 *     };
-	 *     copies: {
-	 *       readonly __select__: number;
-	 *       readonly __insert__: string | number | undefined;
-	 *       readonly __update__: string | number;
-	 *     };
+	 *     },
+	 *     role: {
+	 *       readonly __select__: "admin" | "user";
+	 *       readonly __insert__: "admin" | "user";
+	 *       readonly __update__: "admin" | "user";
+	 *     },
+	 *     createdAt: {
+	 *       readonly __select__: Date;
+	 *       readonly __insert__: Date | undefined;
+	 *       readonly __update__: Date;
+	 *     },
 	 *   };
 	 * };
 	 * ```
