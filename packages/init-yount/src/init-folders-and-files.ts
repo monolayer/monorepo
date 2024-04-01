3eff99436c26739b75b10bc59b4cbdeb923681f8
Marcos Essindi <marcessindi@me.com>
2024-04-01 10:22:55 +0200

    Fixed schema file path in “Next Steps” message

--- packages/init-yount/src/init-folders-and-files.ts
+++ packages/init-yount/src/init-folders-and-files.ts
@@ -92,10 +92,10 @@ export function initFolderAndFiles() {
 		);
 
 		const nextSteps = `1) Edit the database connection details at \`yount.ts\`.
-		2) Run \`npx yount db:create\` to create the database.
-		3) Edit the schema file at \`app/db/schema.ts\`.
-		4) Run 'npx yount generate' to create migrations.
-		5) Run 'npx yount migrate' to migrate the database.`;
+2) Run \`npx yount db:create\` to create the database.
+3) Edit the schema file at \`${path.join(folder.path, "schema.ts")}\`.
+4) Run 'npx yount generate' to create migrations.
+5) Run 'npx yount migrate' to migrate the database.`;
 		p.note(nextSteps, "Next Steps");
 	});
 }
