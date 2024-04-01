f475d15af0e36e4f295de4cd4b187d71d31fb51d
Marcos Essindi <marcessindi@me.com>
2024-04-01 10:04:31 +0200

    Move next steps

--- packages/init-yount/src/bin/cli.ts
+++ packages/init-yount/src/bin/cli.ts
@@ -16,12 +16,6 @@ const program = Effect.succeed("@types/pg").pipe(
 
 const result = await Effect.runPromiseExit(program);
 if (result._tag === "Success") {
-	const nextSteps = `1) Edit the database connection details at \`yount.ts\`.
-2) Run \`npx yount db:create\` to create the database.
-3) Edit the schema file at \`app/db/schema.ts\`.
-4) Run 'npx yount generate' to create migrations.
-5) Run 'npx yount migrate' to migrate the database.`;
-	p.note(nextSteps, "Next Steps");
 	p.outro("Done");
 	exit(0);
 } else {
--- packages/init-yount/src/init-folders-and-files.ts
+++ packages/init-yount/src/init-folders-and-files.ts
@@ -90,6 +90,13 @@ export function initFolderAndFiles() {
 			}),
 			true,
 		);
+
+		const nextSteps = `1) Edit the database connection details at \`yount.ts\`.
+		2) Run \`npx yount db:create\` to create the database.
+		3) Edit the schema file at \`app/db/schema.ts\`.
+		4) Run 'npx yount generate' to create migrations.
+		5) Run 'npx yount migrate' to migrate the database.`;
+		p.note(nextSteps, "Next Steps");
 	});
 }
 
