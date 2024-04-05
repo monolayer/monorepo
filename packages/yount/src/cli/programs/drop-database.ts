1999104c94d30b01dcafebd716291d68d5831270
Marcos Essindi <marcessindi@me.com>
2024-04-05 13:52:32 +0200

    Add option to do a fail safe drop

--- packages/yount/src/cli/programs/drop-database.ts
+++ packages/yount/src/cli/programs/drop-database.ts
@@ -3,14 +3,17 @@ import { Environment } from "../services/environment.js";
 import { spinnerTask } from "../utils/spinner-task.js";
 import { pgQuery } from "./pg-query.js";
 
-export function dropDatabase() {
+export function dropDatabase(failSafe = false) {
 	return Effect.gen(function* (_) {
 		const environment = yield* _(Environment);
 		const database = environment.pg.config.database;
 		const pool = environment.pg.adminPool;
 		return yield* _(
 			spinnerTask(`Drop database ${database}`, () =>
-				pgQuery(pool, `DROP DATABASE ${database};`),
+				pgQuery(
+					pool,
+					`DROP DATABASE ${failSafe ? "IF EXISTS" : ""} ${database};`,
+				),
 			),
 		);
 	});
