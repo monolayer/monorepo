c421c0ee8f802b5a9948cca2ab7d3f1c6ec824af
Marcos Essindi <marcessindi@me.com>
2024-04-08 09:49:13 +0200

    Remove unused function

--- packages/yount/src/cli/utils/clack.ts
+++ packages/yount/src/cli/utils/clack.ts
@@ -1,7 +1,5 @@
 import * as p from "@clack/prompts";
 import color from "picocolors";
-import { exit } from "process";
-import { Config } from "../../config.js";
 
 type LogWithSimpleMessage = typeof p.log & {
 	lineMessage: (message?: string) => void;
@@ -21,22 +19,3 @@ export type Task = {
 	) => string | Promise<string> | void | Promise<void>;
 	enabled?: boolean;
 };
-
-export function checkEnvironmentIsConfigured(
-	config: Config,
-	environment: string,
-	options: { spinner?: ReturnType<typeof p.spinner>; outro?: true } = {},
-) {
-	const envConfig = config.environments[environment];
-	if (envConfig === undefined) {
-		const errorMesage = `Configuration not found for environment '${environment}'. Please check your yount.config.ts file.`;
-		if (options.spinner !== undefined) {
-			options.spinner.stop(errorMesage, 1);
-		} else {
-			p.log.error(errorMesage);
-		}
-		if (options.outro === true) p.outro(`${color.red("Failed")}`);
-		exit(1);
-	}
-	return envConfig;
-}
