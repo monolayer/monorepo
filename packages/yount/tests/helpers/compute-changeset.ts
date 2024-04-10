8a6b5074008ea0297a1d1b6f191e6e58fd26b886
Marcos Essindi <marcessindi@me.com>
2024-04-10 09:29:16 +0200

    Remove unused function

--- packages/yount/tests/helpers/compute-changeset.ts
+++ packages/yount/tests/helpers/compute-changeset.ts
@@ -1,14 +1,10 @@
-import { Effect } from "effect";
 import { Kysely } from "kysely";
 import { changeset } from "~/changeset/changeset.js";
-import { schemaChangeset } from "~/cli/programs/schema-changeset.js";
 import type { CamelCaseOptions } from "~/configuration.js";
 import { localSchema, remoteSchema } from "~/introspection/introspection.js";
 import { createSchemaChangeset } from "~/schema/database_schemas/changeset.js";
 import { schemaInDb } from "~/schema/database_schemas/introspection.js";
 import { Schema, type AnySchema } from "~/schema/schema.js";
-import { layers } from "./layers.js";
-import { programWithErrorCause } from "./run-program.js";
 
 export async function computeChangeset(
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,10 +21,4 @@ export async function computeChangeset(
 		cset.unshift(createSchemaChangeset(schemaName));
 	}
 	return cset;
-}
-
-export async function computeChangeset2() {
-	return Effect.runPromise(
-		Effect.provide(programWithErrorCause(schemaChangeset()), layers),
-	);
 }
