66a14c86949dfa132831909540efe4d79dc74604
Marcos Essindi <marcessindi@me.com>
2024-04-15 11:24:17 +0200

    Use introspectRemoteSchema

--- packages/yount/src/programs/changeset.ts
+++ packages/yount/src/programs/changeset.ts
@@ -1,19 +1,12 @@
-import { Effect, pipe } from "effect";
-import type { Kysely } from "kysely";
+import { Effect } from "effect";
 import { schemaChangeset } from "~/changeset/schema-changeset.js";
 import { createSchemaChangeset } from "~/database/database_schemas/changeset.js";
 import { schemaInDb } from "~/database/database_schemas/introspection.js";
 import { Schema, type AnySchema } from "~/database/schema/schema.js";
-import { dbColumnInfo } from "~/database/schema/table/column/instrospection.js";
-import { dbCheckConstraintInfo } from "~/database/schema/table/constraints/check/introspection.js";
-import { dbForeignKeyConstraintInfo } from "~/database/schema/table/constraints/foreign-key/introspection.js";
-import { dbPrimaryKeyConstraintInfo } from "~/database/schema/table/constraints/primary-key/introspection.js";
-import { dbUniqueConstraintInfo } from "~/database/schema/table/constraints/unique/introspection.js";
-import { dbIndexInfo } from "~/database/schema/table/index/introspection.js";
-import { dbTableInfo } from "~/database/schema/table/introspection.js";
-import { dbTriggerInfo } from "~/database/schema/table/trigger/introspection.js";
-import { dbEnumInfo } from "~/database/schema/types/enum/introspection.js";
-import { introspectLocalSchema } from "~/introspection/introspection.js";
+import {
+	introspectLocalSchema,
+	introspectRemoteSchema,
+} from "~/introspection/introspection.js";
 import { DbClients } from "../services/dbClients.js";
 import { DevEnvironment } from "../services/environment.js";
 
@@ -30,27 +23,28 @@ export function changeset() {
 }
 
 function changesetForLocalSchema(localSchema: AnySchema) {
-	return Effect.all([DevEnvironment, DbClients]).pipe(
-		Effect.flatMap(([devEnvironment, dbClients]) =>
-			Effect.all([
-				Effect.succeed(dbClients.developmentEnvironment.kyselyNoCamelCase),
-				Effect.succeed(devEnvironment.camelCasePlugin),
-				Effect.succeed(Schema.info(localSchema).name || "public"),
-			]),
-		),
+	return context(localSchema).pipe(
 		Effect.flatMap(([kyselyInstance, camelCasePlugin, schemaName]) =>
-			databaseSchema(kyselyInstance, localSchema).pipe(
-				Effect.flatMap((databaseSchema) =>
+			Effect.tryPromise(() =>
+				introspectRemoteSchema(kyselyInstance, schemaName),
+			).pipe(
+				Effect.flatMap((introspectedDatabaseSchema) =>
 					Effect.succeed(
-						schemaChangeset(
-							introspectLocalSchema(
-								localSchema,
-								databaseSchema,
-								camelCasePlugin ?? { enabled: false },
+						introspectLocalSchema(
+							localSchema,
+							introspectedDatabaseSchema,
+							camelCasePlugin,
+						),
+					).pipe(
+						Effect.flatMap((introspectedLocalSchema) =>
+							Effect.succeed(
+								schemaChangeset(
+									introspectedLocalSchema,
+									introspectedDatabaseSchema,
+									schemaName,
+									camelCasePlugin,
+								),
 							),
-							databaseSchema,
-							Schema.info(localSchema).name || "public",
-							camelCasePlugin ?? { enabled: false },
 						),
 					),
 				),
@@ -79,74 +73,14 @@ function connectorSchemas() {
 	);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function databaseSchema(kysely: Kysely<any>, localSchema: AnySchema) {
-	const schemaName = Schema.info(localSchema).name || "public";
-	return pipe(
-		databaseTableInfo(kysely, schemaName),
-		Effect.flatMap(tableList),
-		Effect.flatMap((tables) => databaseInfo(kysely, schemaName, tables)),
-	);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function databaseTableInfo(kysely: Kysely<any>, schemaName = "public") {
-	return Effect.tryPromise(async () => await dbTableInfo(kysely, schemaName));
-}
-
-function tableList(tables: Awaited<ReturnType<typeof dbTableInfo>>) {
-	const tableList = tables.reduce<string[]>((acc, table) => {
-		if (table.name !== null) acc.push(table.name);
-		return acc;
-	}, []);
-	return Effect.succeed(tableList);
-}
-
-function databaseInfo(
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	kysely: Kysely<any>,
-	schema: string,
-	tables: string[],
-) {
-	return Effect.all([
-		Effect.tryPromise(async () => await dbColumnInfo(kysely, schema, tables)),
-		Effect.tryPromise(async () => await dbIndexInfo(kysely, schema, tables)),
-		Effect.tryPromise(
-			async () => await dbUniqueConstraintInfo(kysely, schema, tables),
-		),
-		Effect.tryPromise(
-			async () => await dbForeignKeyConstraintInfo(kysely, schema, tables),
-		),
-		Effect.tryPromise(
-			async () => await dbPrimaryKeyConstraintInfo(kysely, schema, tables),
-		),
-		Effect.tryPromise(async () => await dbTriggerInfo(kysely, schema, tables)),
-		Effect.tryPromise(async () => await dbEnumInfo(kysely, schema)),
-		Effect.tryPromise(
-			async () => await dbCheckConstraintInfo(kysely, schema, tables),
-		),
-	]).pipe(
-		Effect.flatMap(
-			([
-				columns,
-				indexes,
-				uniqueConstraints,
-				foreignKeys,
-				primaryKeys,
-				triggers,
-				enums,
-				checkConstraints,
-			]) =>
-				Effect.succeed({
-					table: columns,
-					index: indexes,
-					foreignKeyConstraints: foreignKeys,
-					uniqueConstraints: uniqueConstraints,
-					checkConstraints: checkConstraints,
-					primaryKey: primaryKeys,
-					triggers: triggers,
-					enums: enums,
-				}),
+function context(localSchema: AnySchema) {
+	return Effect.all([DevEnvironment, DbClients]).pipe(
+		Effect.flatMap(([devEnvironment, dbClients]) =>
+			Effect.all([
+				Effect.succeed(dbClients.developmentEnvironment.kyselyNoCamelCase),
+				Effect.succeed(devEnvironment.camelCasePlugin || { enabled: false }),
+				Effect.succeed(Schema.info(localSchema).name || "public"),
+			]),
 		),
 	);
 }
