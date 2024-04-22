82fdbde4441b7c47261411b49bcd366843f3a3f7
Marcos Essindi <marcessindi@me.com>
2024-04-22 13:21:26 +0200

    Output relative paths

--- packages/yount/src/programs/handle-pending-migrations.ts
+++ packages/yount/src/programs/handle-pending-migrations.ts
@@ -2,7 +2,9 @@ import * as p from "@clack/prompts";
 import { confirm } from "@clack/prompts";
 import { Effect } from "effect";
 import { unlinkSync } from "fs";
+import path from "path";
 import color from "picocolors";
+import { cwd } from "process";
 import { cancelOperation } from "./cancel-operation.js";
 import { localPendingMigrations } from "./local-pending-migrations.js";
 
@@ -38,7 +40,9 @@ function logPendingMigrations(
 		Effect.tap(() =>
 			Effect.forEach(pending, (pendingMigration) =>
 				Effect.succeed(
-					p.log.warn(`${color.yellow("pending")} ${pendingMigration.path}`),
+					p.log.warn(
+						`${color.yellow("pending")} ${path.relative(cwd(), pendingMigration.path)}`,
+					),
 				),
 			),
 		),
@@ -68,7 +72,9 @@ function deletePendingMigrations(
 				Effect.succeed(true).pipe(
 					Effect.map(() => {
 						unlinkSync(pendingMigration.path);
-						p.log.info(`${color.green("removed")} (${pendingMigration.path})`);
+						p.log.info(
+							`${color.green("removed")} ${path.relative(cwd(), pendingMigration.path)}`,
+						);
 					}),
 				),
 			),
--- packages/yount/src/utils.ts
+++ packages/yount/src/utils.ts
@@ -1,6 +1,7 @@
 import * as p from "@clack/prompts";
 import { createHash } from "crypto";
 import { existsSync, writeFileSync } from "fs";
+import path from "path";
 import color from "picocolors";
 
 export function createFile(path: string, content: string, log = true): void {
@@ -11,8 +12,11 @@ export function createFile(path: string, content: string, log = true): void {
 	}
 }
 
-function logCreation(path: string, overwritten = false): void {
-	p.log.info(`${color.green(overwritten ? "overwritten" : "created")} ${path}`);
+function logCreation(filePath: string, overwritten = false): void {
+	const relativePath = path.relative(process.cwd(), filePath);
+	p.log.info(
+		`${color.green(overwritten ? "overwritten" : "created")} ${relativePath}`,
+	);
 }
 
 export function hashValue(value: string) {
