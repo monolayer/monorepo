116e55842518ca5614a1905bdf74faaa9ed20ee1
Marcos Essindi <marcessindi@me.com>
2024-04-23 16:21:26 +0200

    Add changeset stats

---
+++ packages/yount/src/changeset/changeset-stats.ts
@@ -0,0 +1,291 @@
+/* eslint-disable max-lines */
+import { ChangeSetType, type Changeset } from "./types.js";
+
+type SummaryStats = { added: number; dropped: number; altered: number };
+
+type SchemaStats = {
+	tableSummary: SummaryStats;
+	tables: Record<string, TableStats>;
+	enumTypes: SummaryStats;
+};
+
+type TableStats = {
+	columns: SummaryStats;
+	primaryKeys: SummaryStats;
+	foreignKeys: SummaryStats;
+	uniqueConstraints: SummaryStats;
+	checkConstraints: SummaryStats;
+	indexes: SummaryStats;
+	triggers: SummaryStats;
+};
+
+type ChangesetStats = {
+	database: DbStats;
+	databaseSchemas: Record<string, SchemaStats>;
+};
+
+type DbStats = {
+	extensions: {
+		added: number;
+		dropped: number;
+	};
+	schemaSummary: { added: number; dropped: number };
+};
+
+const dbStatsFn: Record<
+	string,
+	{
+		key: keyof DbStats;
+		op: "add" | "drop";
+	}
+> = {
+	[`${ChangeSetType.CreateSchema}`]: { key: "schemaSummary", op: "add" },
+	[`${ChangeSetType.CreateExtension}`]: { key: "extensions", op: "add" },
+	[`${ChangeSetType.DropExtension}`]: { key: "extensions", op: "drop" },
+};
+
+const tableStatsFn: Record<
+	string,
+	{
+		key: keyof TableStats;
+		op: "add" | "drop" | "alter";
+	}
+> = {
+	[`${ChangeSetType.CreatePrimaryKey}`]: { key: "primaryKeys", op: "add" },
+	[`${ChangeSetType.DropPrimaryKey}`]: { key: "primaryKeys", op: "drop" },
+	[`${ChangeSetType.CreateForeignKey}`]: { key: "foreignKeys", op: "add" },
+	[`${ChangeSetType.DropForeignKey}`]: { key: "foreignKeys", op: "drop" },
+	[`${ChangeSetType.ChangeForeignKey}`]: { key: "foreignKeys", op: "alter" },
+	[`${ChangeSetType.CreateColumn}`]: { key: "columns", op: "add" },
+	[`${ChangeSetType.DropColumn}`]: { key: "columns", op: "drop" },
+	[`${ChangeSetType.ChangeColumn}`]: { key: "columns", op: "alter" },
+	[`${ChangeSetType.CreateIndex}`]: { key: "indexes", op: "add" },
+	[`${ChangeSetType.ChangeIndex}`]: { key: "indexes", op: "alter" },
+	[`${ChangeSetType.DropIndex}`]: { key: "indexes", op: "drop" },
+	[`${ChangeSetType.CreateTrigger}`]: { key: "triggers", op: "add" },
+	[`${ChangeSetType.DropTrigger}`]: { key: "triggers", op: "drop" },
+	[`${ChangeSetType.UpdateTrigger}`]: { key: "triggers", op: "alter" },
+	[`${ChangeSetType.ChangeColumnName}`]: { key: "columns", op: "alter" },
+	[`${ChangeSetType.CreateUnique}`]: { key: "uniqueConstraints", op: "add" },
+	[`${ChangeSetType.DropUnique}`]: { key: "uniqueConstraints", op: "drop" },
+	[`${ChangeSetType.ChangeUnique}`]: { key: "uniqueConstraints", op: "alter" },
+	[`${ChangeSetType.CreateCheck}`]: { key: "checkConstraints", op: "add" },
+	[`${ChangeSetType.ChangeCheck}`]: { key: "checkConstraints", op: "alter" },
+	[`${ChangeSetType.DropCheck}`]: { key: "checkConstraints", op: "drop" },
+};
+
+type TableAlterations = Record<string, Record<string, boolean>>;
+
+export function changesetStats(changesets: Changeset[]) {
+	const stats: ChangesetStats = {
+		database: {
+			extensions: { added: 0, dropped: 0 },
+			schemaSummary: { added: 0, dropped: 0 },
+		},
+		databaseSchemas: {},
+	};
+
+	const tableAlterations: TableAlterations = {};
+	const result = changesets.reduce((acc, changeset) => {
+		for (const key in dbStatsFn) {
+			if (key === changeset.type) {
+				const keyAndOp = dbStatsFn[key as keyof typeof dbStatsFn]!;
+				processDbStats(acc, keyAndOp.key, keyAndOp.op);
+				return acc;
+			}
+		}
+		if (addEnumStats(changeset, acc)) return acc;
+		if (addTableStats(changeset, acc, tableAlterations)) return acc;
+
+		for (const key in tableStatsFn) {
+			if (key === changeset.type) {
+				const keyAndOp = tableStatsFn[key]!;
+				processTableStats(
+					changeset,
+					acc,
+					keyAndOp.key,
+					changeset.type,
+					keyAndOp.op,
+					tableAlterations,
+				);
+			}
+		}
+		return acc;
+	}, stats);
+	return result;
+}
+
+export function processDbStats(
+	changesetStats: ChangesetStats,
+	statName: keyof DbStats,
+	op: "add" | "drop" | "alter",
+) {
+	switch (op) {
+		case "add":
+			changesetStats.database[statName].added++;
+			break;
+		case "drop":
+			changesetStats.database[statName].dropped++;
+			break;
+		default:
+			return false;
+	}
+}
+
+function initSchemaStats(
+	schemas: Record<string, SchemaStats>,
+	schemaName: string,
+) {
+	if (schemas[schemaName] === undefined) {
+		schemas[schemaName] = {
+			enumTypes: { added: 0, dropped: 0, altered: 0 },
+			tableSummary: { added: 0, dropped: 0, altered: 0 },
+			tables: {},
+		};
+	}
+	return schemas[schemaName];
+}
+
+function addEnumStats(changeset: Changeset, stats: ChangesetStats) {
+	if (changeset.schemaName === null) {
+		return false;
+	} else {
+		const schemaStats = initSchemaStats(
+			stats.databaseSchemas,
+			changeset.schemaName,
+		);
+		switch (changeset.type) {
+			case ChangeSetType.CreateEnum:
+				schemaStats.enumTypes.added++;
+				break;
+			case ChangeSetType.DropEnum:
+				schemaStats.enumTypes.dropped++;
+				break;
+			case ChangeSetType.ChangeEnum:
+				schemaStats.enumTypes.altered++;
+				break;
+			default:
+				return false;
+		}
+		return true;
+	}
+}
+
+function addTableAlteration(
+	changeset: Changeset,
+	stats: SummaryStats,
+	tableAlterations: TableAlterations,
+) {
+	const schemaName = changeset.schemaName!;
+	const tableName = changeset.tableName;
+	tableAlterations[schemaName] = tableAlterations[schemaName] || {};
+	tableAlterations[schemaName][tableName] =
+		tableAlterations[schemaName][tableName] || false;
+	if (tableAlterations[schemaName][tableName] === false) {
+		stats.altered++;
+		tableAlterations[schemaName][tableName] = true;
+	}
+}
+
+function addTableStats(
+	changeset: Changeset,
+	stats: ChangesetStats,
+	tableAlterations: TableAlterations,
+) {
+	if (changeset.schemaName === null) {
+		return false;
+	} else {
+		const schemaStats = initSchemaStats(
+			stats.databaseSchemas,
+			changeset.schemaName,
+		);
+		switch (changeset.type) {
+			case ChangeSetType.CreateTable:
+				schemaStats.tableSummary.added++;
+				break;
+			case ChangeSetType.DropTable:
+				schemaStats.tableSummary.dropped++;
+				break;
+			case ChangeSetType.ChangeTable:
+				addTableAlteration(
+					changeset,
+					schemaStats.tableSummary,
+					tableAlterations,
+				);
+				break;
+			default:
+				return false;
+		}
+		return true;
+	}
+}
+
+function initTableStats(tables: Record<string, TableStats>, tableName: string) {
+	if (tables[tableName] === undefined) {
+		tables[tableName] = {
+			columns: { added: 0, dropped: 0, altered: 0 },
+			primaryKeys: { added: 0, dropped: 0, altered: 0 },
+			foreignKeys: { added: 0, dropped: 0, altered: 0 },
+			uniqueConstraints: { added: 0, dropped: 0, altered: 0 },
+			checkConstraints: { added: 0, dropped: 0, altered: 0 },
+			indexes: { added: 0, dropped: 0, altered: 0 },
+			triggers: { added: 0, dropped: 0, altered: 0 },
+		};
+	}
+	return tables[tableName];
+}
+
+export function processTableStats(
+	changeset: Changeset,
+	changesetStats: ChangesetStats,
+	statName: keyof TableStats,
+	match: ChangeSetType,
+	op: "add" | "drop" | "alter",
+	tableAlterations: TableAlterations,
+) {
+	if (changeset.tableName === "none") {
+		return false;
+	}
+	const tableName = changeset.tableName;
+	if (changeset.schemaName === null) {
+		return false;
+	} else {
+		const schemaStats = initSchemaStats(
+			changesetStats.databaseSchemas,
+			changeset.schemaName,
+		);
+		const stats = initTableStats(schemaStats.tables, tableName);
+
+		if (changeset.type === match) {
+			switch (op) {
+				case "add":
+					addTableAlteration(
+						changeset,
+						schemaStats.tableSummary,
+						tableAlterations,
+					);
+					stats[statName].added++;
+					return true;
+				case "drop":
+					addTableAlteration(
+						changeset,
+						schemaStats.tableSummary,
+						tableAlterations,
+					);
+					stats[statName].dropped++;
+					return true;
+				case "alter":
+					addTableAlteration(
+						changeset,
+						schemaStats.tableSummary,
+						tableAlterations,
+					);
+					stats[statName].altered++;
+					return true;
+				default:
+					break;
+			}
+		}
+		return false;
+	}
+}
