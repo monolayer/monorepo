24c569262c507995dd002e9caa88d2889472213e
Marcos Essindi <marcessindi@me.com>
2024-04-25 11:46:56 +0200

    Use NODE_ENV instead of APP_ENV

--- packages/init-yount/src/init-folders-and-files.ts
+++ packages/init-yount/src/init-folders-and-files.ts
@@ -144,7 +144,7 @@ import configuration from "./configuration";
 import { type DB } from "./schema";
 
 export const defaultDb = new Kysely<DB>(
-	kyselyConfig(configuration, process.env.APP_ENV || "development")
+	kyselyConfig(configuration, process.env.NODE_ENV || "development")
 );
 `);
 
