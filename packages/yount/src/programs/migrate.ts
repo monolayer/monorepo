956b9a5345f6466a92fadec77cc792b7a1835cb7
Marcos Essindi <marcessindi@me.com>
2024-04-27 10:39:00 +0200

    Rename file

--- packages/yount/src/programs/migrate.ts
+++ packages/yount/src/programs/migrate.ts
@@ -2,7 +2,7 @@ import * as p from "@clack/prompts";
 import { Effect } from "effect";
 import { Migrator } from "../services/migrator.js";
 import { logMigrationResultStatus } from "./log-migration-result-status.js";
-import { validateRevisionDependencies } from "./revision-dependencies.js";
+import { validateRevisionDependencies } from "./validate-revision-dependencies.js";
 
 export function migrate() {
 	return Migrator.pipe(
