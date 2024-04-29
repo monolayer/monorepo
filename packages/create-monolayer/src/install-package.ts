b2cf7c27ceb7b66a73e87b14b85e702390496332
Marcos Essindi <marcessindi@me.com>
2024-04-29 14:16:07 +0200

    Add package install type

--- packages/create-monolayer/src/install-package.ts
+++ packages/create-monolayer/src/install-package.ts
@@ -2,7 +2,10 @@ import * as p from "@clack/prompts";
 import { Effect } from "effect";
 import { execa } from "execa";
 import { checkPackageInstallation } from "./check-package-installation.js";
-import { PackageInstallEnvironment } from "./select-package-manager.js";
+import {
+	PackageInstallEnvironment,
+	type PackageManager,
+} from "./select-package-manager.js";
 
 export function installPackage(
 	name: string,
@@ -19,26 +22,27 @@ export function installPackage(
 	);
 }
 
-function install(options: { packageName: string; dev: boolean }) {
+interface InstallOptions {
+	packageName: string;
+	dev: boolean;
+}
+
+function install(options: InstallOptions) {
 	return PackageInstallEnvironment.pipe(
 		Effect.tap((env) =>
 			Effect.tryPromise(async () => {
 				const s = p.spinner();
-				s.start(
-					`Installing ${options.packageName} via ${env.packageManager.name}`,
-				);
+				s.start(`Installing ${installText(options, env.packageManager)}`);
 				try {
 					const args = [env.packageManager.addCommand, options.packageName];
 					if (options.dev) {
 						args.push(env.packageManager.saveDevFlag);
 					}
 					await execa(env.packageManager.name, args);
-					s.stop(
-						`Installed ${options.packageName} via ${env.packageManager.name}.`,
-					);
+					s.stop(`Installed ${installText(options, env.packageManager)}.`);
 				} catch (error) {
 					s.stop(
-						`Failed to install ${options.packageName} via ${env.packageManager.name}.`,
+						`Failed to install ${installText(options, env.packageManager)}.`,
 						1,
 					);
 					throw error;
@@ -47,4 +51,11 @@ function install(options: { packageName: string; dev: boolean }) {
 			}),
 		),
 	);
+}
+
+function installText(
+	installOptions: InstallOptions,
+	packageManager: PackageManager,
+) {
+	return `${installOptions.packageName} to ${installOptions.dev ? "devDependencies" : "dependencies"} via ${packageManager.name}`;
 }
