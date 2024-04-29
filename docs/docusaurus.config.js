690c83328c105acf1a83ae67b14e7747c74f95d0
Marcos Essindi <marcessindi@me.com>
2024-04-29 16:14:44 +0200

    Remove blog preset

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -58,13 +58,6 @@ const config = {
 						}
 					},
 				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-				},
 				theme: {
 					customCss: "./src/css/custom.css",
 				},
