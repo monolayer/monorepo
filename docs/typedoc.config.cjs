ff4d8903afea84d8ad77d583d872c18d65710d1e
Marcos Essindi <marcessindi@me.com>
2024-04-29 15:16:50 +0200

    Modify typedoc config

--- docs/typedoc.config.cjs
+++ docs/typedoc.config.cjs
@@ -1,3 +1,9 @@
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
+  "enumMembersFormat": "table",
+  "parametersFormat": "table",
+  "propertiesFormat": "table",
+  "typeDeclarationFormat": "list",
+  "indexFormat": "list",
+  "outputFileStrategy": "members",
 };
