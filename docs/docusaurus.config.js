5a1b26b6f547dea6a3f02f391cea956f9d47281f
Marcos Essindi <marcessindi@me.com>
2024-04-29 16:15:28 +0200

    Change title

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "My Site",
-	tagline: "Dinosaurs are cool",
+	title: "monolayer",
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
