37de3b435e13288b1d3abe5940ad8de6e2a21347
Marcos Essindi <marcessindi@me.com>
2024-04-30 10:11:16 +0200

    Migrate docusaurus to TypeScript

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,8 +3,8 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
