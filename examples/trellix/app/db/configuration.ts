53316e67e2a60550c1034c16df182fb476c4d979
Marcos Essindi <marcessindi@me.com>
2024-05-16 12:23:41 +0200

    Configure development db connection

--- examples/trellix/app/db/configuration.ts
+++ examples/trellix/app/db/configuration.ts
@@ -1,26 +1,22 @@
 import { type Configuration } from "monolayer/config";
 import { dbSchema } from "./schema";
 import { dbExtensions } from "./extensions";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default {
-	schemas: [dbSchema],
-	extensions: dbExtensions,
-	connections: {
-		development: {
-			// With credentials
-			database: "#database_development",
-			user: "#user",
-			password: "#password",
-			host: "#host",
-			port: 5432,
-			// With connection string
-			// connectionString: "#connection_string",
-		},
-		production: {
-			connectionString: process.env.DATABASE_URL,
-		}
-	},
-	camelCasePlugin: {
-		enabled: false,
-	},
+  schemas: [dbSchema],
+  extensions: dbExtensions,
+  connections: {
+    development: {
+      connectionString: process.env.DEV_DATABASE_URL,
+    },
+    production: {
+      connectionString: process.env.DATABASE_URL,
+    },
+  },
+  camelCasePlugin: {
+    enabled: false,
+  },
 } satisfies Configuration;
