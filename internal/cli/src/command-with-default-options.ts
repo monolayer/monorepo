99b5195e2b0e1f9679eaee39f6a0f8615c6d989e
Marcos Essindi <marcessindi@me.com>
2024-08-12 12:48:02 +0200

    Add default --env-file option

--- internal/cli/src/command-with-default-options.ts
+++ internal/cli/src/command-with-default-options.ts
@@ -18,5 +18,9 @@ export function commandWithDefaultOptions({
 			"-c, --connection <connection-name>" as const,
 			"configuration connection name as defined in configuration.ts" as const,
 			"development" as const,
+		)
+		.option(
+			"-e, --env-file <env-file>" as const,
+			"load configuration from .env file" as const,
 		);
 }
