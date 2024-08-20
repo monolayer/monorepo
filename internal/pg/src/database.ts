3d2e12d80fcc05a4a4e95982942134dd5a21118e
Marcos Essindi <marcessindi@me.com>
2024-08-20 17:16:55 +0200

    Add inline documentation

--- internal/pg/src/database.ts
+++ internal/pg/src/database.ts
@@ -23,10 +23,25 @@ export function defineDatabase(config: DatabaseConfig) {
  * @category Classes
  */
 export class MonoLayerPgDatabase {
+	/**
+	 * Id of the database
+	 */
 	id: string;
+	/**
+	 * Schemas in the database
+	 */
 	schemas: AnySchema[];
-	generatePrismaSchema: boolean;
+	/**
+	 * Extensions to install in the database
+	 */
 	extensions?: PgExtension[];
+	/**
+	 * Whether to generate a Prisma schema for the database after running migrations.
+	 */
+	generatePrismaSchema: boolean;
+	/**
+	 * Whether to convert camelCase column names to snake_case column names in the database.
+	 */
 	camelCase: boolean;
 
 	constructor(config: DatabaseConfig) {
@@ -37,6 +52,26 @@ export class MonoLayerPgDatabase {
 		this.camelCase = config.camelCase ?? false;
 	}
 
+	/**
+	 * Get the connection string for the database.
+	 *
+	 * It will fetch he connection string from by inferring the environment variable in the following format:
+	 *
+	 * `MONO_PG_${DATABASE_ID_TO_UPPER_CASE_AND_SNAKE_CASE}_DATABASE_URL`.
+	 *
+	 * @example
+	 * For the following database:
+	 *
+	 * ```ts
+	 * const db = defineDatabase({
+	 * 	id: "my-db",
+	 * 	schemas: [],
+	 * });
+	 * ```
+	 *
+	 * The connection string will be fetched from the environment variable:
+	 * `MONO_PG_MY_DB_DATABASE_URL`
+	 */
 	get connectionString() {
 		const envVar = `MONO_PG_${snakeCase(this.id).toUpperCase()}_DATABASE_URL`;
 		const envConnectionString = process.env[envVar];
