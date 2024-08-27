51d5a10011cd93a48e1ce04c595f6c3c5fb5b525
Marcos Essindi <marcessindi@me.com>
2024-08-27 08:29:46 +0200

    Print warnings when generating migrations

--- internal/programs/src/migrations/render.ts
+++ internal/programs/src/migrations/render.ts
@@ -1,4 +1,5 @@
 import * as p from "@clack/prompts";
+import { printWarnings } from "@monorepo/pg/changeset/summary.js";
 import type { Changeset } from "@monorepo/pg/changeset/types.js";
 import { Migrator } from "@monorepo/services/migrator.js";
 import { pipe } from "effect";
@@ -12,6 +13,11 @@ export const render = (changeset: Changeset[]) =>
 		flatMap(([migrator, migrationName]) =>
 			migrator.renderChangesets(changeset, migrationName),
 		),
+		tap(() => {
+			printWarnings(
+				changeset.flatMap((c) => (c.warnings !== undefined ? c.warnings : [])),
+			);
+		}),
 		tap((migration) =>
 			forEach(migration, (migration) =>
 				p.log.info(`Generated migration: ${migration}`),
