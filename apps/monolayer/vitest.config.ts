0b126d63d55aee644595ad2c4823e1fdbc9beb70
Marcos Essindi <marcessindi@me.com>
2024-09-12 14:43:44 +0200

    Update workflow

--- apps/monolayer/vitest.config.ts
+++ apps/monolayer/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		name: "monolayer",
+		name: "monolayer-pg",
 		pool: "forks",
 		poolOptions: {
 			forks: {
