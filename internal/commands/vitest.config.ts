486754c754f536af37c582915f5901b31c894e8c
Marcos Essindi <marcessindi@me.com>
2024-09-16 17:07:49 +0200

    Add commands package

---
+++ internal/commands/vitest.config.ts
@@ -0,0 +1,23 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+const config = defineConfig({
+	test: {
+		name: "commands",
+		coverage: {
+			provider: "istanbul",
+			reporter: ["html", "json"],
+			exclude: ["**/tmp/**", "**/tests/**", "**/docs/**"],
+		},
+		setupFiles: ["__test_setup__/setup.ts"],
+		reporters: ["default"],
+	},
+	server: {
+		watch: {
+			ignored: ["**/node_modules/**", "**/dist/**", "**/tmp/**", "**/docs/**"],
+		},
+	},
+	plugins: [tsconfigPaths()],
+});
+
+export default config;
