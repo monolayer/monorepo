ae56974bdeb748e33cea55c3cd78f91cb882de06
Marcos Essindi <marcessindi@me.com>
2024-10-16 11:28:08 +0200

    Add packages to test workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,6 +1,7 @@
 export default [
 	"apps/*/vitest.config.ts",
 	"internal/*/vitest.config.ts",
+	"packages/*/vitest.config.ts",
 	"private/*/vitest.config.ts",
 	"private-apps/*/vitest.config.ts",
 ];
