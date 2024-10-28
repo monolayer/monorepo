bf8c91d03132d7f55f65b44b23c2e49fcb2253cc
Marcos Essindi <marcessindi@me.com>
2024-10-28 10:30:37 +0100

    Add hashDigest effect

--- internal/utils/src/hash-value.ts
+++ internal/utils/src/hash-value.ts
@@ -1,7 +1,30 @@
-import { createHash } from "crypto";
+import { gen, succeed } from "effect/Effect";
+import { createHash } from "node:crypto";
 
 export function hashValue(value: string) {
 	const hash = createHash("sha256");
 	hash.update(value);
 	return hash.digest("hex").substring(0, 8);
+}
+
+interface HashDigestOptions {
+	/**
+	 * Length of the digest to generate.
+	 *
+	 * @default 8
+	 */
+	length: number;
+}
+/**
+ * Returns a hex digest of a string.
+ * @param value String to generate a digest.
+ * @param options {@link HashDigestOptions}
+ */
+export function hashDigest(value: string, options?: HashDigestOptions) {
+	return gen(function* () {
+		const hash = createHash("sha256");
+		hash.update(value);
+		const digest = hash.digest("hex").substring(0, options?.length ?? 8);
+		return yield* succeed(digest);
+	});
 }
