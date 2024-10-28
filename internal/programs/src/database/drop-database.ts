afc8f1b05f7b2513e8728dc65b3363b959289893
Marcos Essindi <marcessindi@me.com>
2024-10-28 10:42:05 +0100

    Remove spinner

--- internal/programs/src/database/drop-database.ts
+++ internal/programs/src/database/drop-database.ts
@@ -1,11 +1,9 @@
-import { spinnerTask } from "@monorepo/cli/spinner-task.js";
 import { DbClients } from "@monorepo/services/db-clients.js";
 import { adminPgQuery } from "@monorepo/services/db-clients/admin-pg-query.js";
 import { Effect } from "effect";
 
 export const dropDatabase = Effect.gen(function* () {
 	const clients = yield* DbClients;
-	yield* spinnerTask(`Drop database ${clients.databaseName}`, () =>
-		adminPgQuery(`DROP DATABASE IF EXISTS "${clients.databaseName}";`),
-	);
+	yield* adminPgQuery(`DROP DATABASE IF EXISTS "${clients.databaseName}";`);
+	return clients.databaseName;
 });
