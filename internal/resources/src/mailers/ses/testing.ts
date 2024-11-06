6b9b5b2173233f199f36f9ce65e3e80f7c793970
Marcos Essindi <marcessindi@me.com>
2024-11-06 18:39:02 +0100

    Update packages

--- internal/resources/src/mailers/ses/testing.ts
+++
@@ -1,5 +0,0 @@
-export async function sesEmails(port: number) {
-	const response = await fetch(`http://localhost:${port}/store`);
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return ((await response.json()) as any).emails;
-}
--- internal/resources/src/read-env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import path from "node:path";
-import { cwd } from "node:process";
-
-export function readEnvVar(envVarName: string) {
-	const envObj: Record<string, string> = {};
-	dotenv.config({ path: path.join(cwd(), ".env.f4"), processEnv: envObj });
-	return envObj[envVarName] ?? process.env[envVarName] ?? "";
-}
