d7f1ccb0f2c759468164f5f2dd219ed473e7f07f
Marcos Essindi <marcessindi@me.com>
2024-11-11 11:52:48 +0100

    Add database drop warning

--- internal/db/src/actions/db/drop.ts
+++ internal/db/src/actions/db/drop.ts
@@ -1,8 +1,12 @@
 import type { Command } from "@commander-js/extra-typings";
 import { commandWithDefaultOptions } from "@monorepo/cli/command-with-default-options.js";
 import { dropDatabase } from "@monorepo/programs/database/drop-database.js";
-import { catchAll, fail, gen, tap } from "effect/Effect";
+import { DbClients } from "@monorepo/services/db-clients.js";
+import { catchAll, fail, gen, tryPromise } from "effect/Effect";
+import { exit } from "node:process";
 import ora from "ora";
+import color from "picocolors";
+import prompts from "prompts";
 import { headlessCliAction } from "~db/cli-action.js";
 
 export function dropDb(program: Command) {
@@ -15,12 +19,12 @@ export function dropDb(program: Command) {
 			const spinner = ora();
 			await headlessCliAction(opts, [
 				gen(function* () {
+					const dbName = yield* databaseName;
+					yield* dropWarning(dbName);
 					spinner.start("Droppping database");
-					return yield* dropDatabase;
+					yield* dropDatabase;
+					spinner.succeed(`Dropped database: ${dbName}`);
 				}).pipe(
-					tap((databaseName) =>
-						spinner.succeed(`Dropped database: ${databaseName}`),
-					),
 					catchAll((e) => {
 						spinner.fail(`Failed to drop database.`);
 						return fail(e);
@@ -29,3 +33,28 @@ export function dropDb(program: Command) {
 			]);
 		});
 }
+
+const databaseName = gen(function* () {
+	const clients = yield* DbClients;
+	return clients.databaseName;
+});
+
+export const dropWarning = (databaseName: string) =>
+	tryPromise(async () => {
+		let aborted = false;
+		console.log(
+			`${color.yellow("Warning")} You are about to delete the database \`${databaseName}\`.`,
+		);
+		const response = await prompts({
+			type: "confirm",
+			name: "value",
+			message: "Do you want to proceed?",
+			initial: true,
+			onState: (e) => {
+				aborted = e.aborted;
+			},
+		});
+		if (aborted || response.value === false) {
+			exit(1);
+		}
+	});
