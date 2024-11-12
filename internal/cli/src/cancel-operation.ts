312247a77a7fad3db76764b6656d73e132b82844
Marcos Essindi <marcessindi@me.com>
2024-11-12 19:07:37 +0100

    Less verbosity when canceling an operation

--- internal/cli/src/cancel-operation.ts
+++ internal/cli/src/cancel-operation.ts
@@ -1,10 +1,6 @@
 import { Effect } from "effect";
-import color from "picocolors";
 import { exit } from "process";
 
 export function cancelOperation() {
-	return Effect.void.pipe(
-		Effect.tap(() => console.log(color.red("Operation cancelled."))),
-		Effect.tap(() => Effect.fail(exit(1))),
-	);
+	return Effect.void.pipe(Effect.tap(() => Effect.fail(exit(1))));
 }
