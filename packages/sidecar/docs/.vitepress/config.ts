7b137a937e4ddf542b0ed317bdb8f395133e321c
Marcos Essindi <marcessindi@me.com>
2024-11-15 08:35:34 +0100

    Update docs

--- packages/sidecar/docs/.vitepress/config.ts
+++ packages/sidecar/docs/.vitepress/config.ts
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Sidecar",
-  description: "A modern backend framework",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+	title: "Sidecar",
+	description: "A modern backend framework",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Guide", link: "/guide/markdown-examples" },
+			{ text: "API Reference", link: "/reference/api/globals" },
+		],
+		sidebar: {
+			"/guide/": {
+				base: "/guide",
+				items: [{ text: "Markdown Examples", link: "markdown-examples" }],
+			},
+			"/reference/": {
+				base: "/reference",
+				items: require("./../reference/api/typedoc-sidebar.json"),
+			},
+		},
+		socialLinks: [
+			{ icon: "github", link: "https://github.com/vuejs/vitepress" },
+		],
+	},
+});
