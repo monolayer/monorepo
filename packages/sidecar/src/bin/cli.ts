0ddb9ef053cb48f612fbd6b17a787b1028fe160d
Marcos Essindi <marcessindi@me.com>
2024-11-15 08:24:29 +0100

    Add CLI interface

--- packages/sidecar/src/bin/cli.ts
+++ packages/sidecar/src/bin/cli.ts
@@ -1,3 +1,26 @@
 #!/usr/bin/env tsx
+import type { Command as CommandExtra } from "@commander-js/extra-typings";
+import { Command, CommanderError } from "commander";
+import { exit } from "process";
 
-console.log("Hello from sidecar");
+function isCommanderError(error: unknown): error is CommanderError {
+	return error instanceof CommanderError;
+}
+
+async function main() {
+	const program = new Command() as unknown as CommandExtra;
+
+	program.name("sidecar").version("1.0.0");
+
+	program.exitOverride();
+
+	try {
+		program.parse();
+	} catch (err) {
+		if (isCommanderError(err) && err.code === "commander.help") {
+			exit(0);
+		}
+	}
+}
+
+main().catch(console.error);
