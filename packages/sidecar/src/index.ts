bf23cbb960f9789c913847260fff64cc4b3407c4
Marcos Essindi <marcessindi@me.com>
2024-11-15 10:09:28 +0100

    Add exports

--- packages/sidecar/src/index.ts
+++ packages/sidecar/src/index.ts
@@ -1,9 +1,11 @@
 export {
+	Container,
 	MappedPort,
 	ResourceContainer,
 	SidecarContainer,
 	StartOptions,
 } from "./container.js";
+export { RedisContainer } from "./resources/redis/redis-container.js";
 export { Redis } from "./resources/redis/redis.js";
 export { Resource, ResourceOptions } from "./resources/resource.js";
 export { startTestContainer } from "./start-test-container.js";
