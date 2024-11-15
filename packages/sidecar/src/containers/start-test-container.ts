b6dbcf61a0023d237147411e10abe9c1d580f755
Marcos Essindi <marcessindi@me.com>
2024-11-15 14:41:09 +0100

    Remove LocalStack from startTestContainer

--- packages/sidecar/src/containers/start-test-container.ts
+++ packages/sidecar/src/containers/start-test-container.ts
@@ -1,8 +1,6 @@
 import type { SidecarContainer } from "~sidecar/containers/container.js";
-import { LocalStackContainer } from "~sidecar/containers/local-stack.js";
 import { RedisContainer } from "~sidecar/containers/redis.js";
 import type { ContainerizedResource } from "~sidecar/resources/interfaces.js";
-import type { LocalStack } from "~sidecar/resources/local-stack.js";
 import type { Redis } from "~sidecar/resources/redis.js";
 
 export async function startTestContainer(
@@ -15,13 +13,7 @@ export async function startTestContainer(
 	const id = resource.id;
 
 	if (isRedis(resource)) {
-		console.log("IS REDIS", resource);
 		container = new RedisContainer(resource, `${resource.id}-test`);
-		return await container.start();
-	}
-	if (isLocalStack(resource)) {
-		console.log("isLocalStack", resource);
-		container = new LocalStackContainer(resource, `${resource.id}-test`);
 		return await container.start();
 	}
 	if (container === undefined) {
@@ -32,9 +24,4 @@ export async function startTestContainer(
 function isRedis<C>(resource: unknown): resource is Redis<C> {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	return (resource as any).constructor.name === "Redis";
-}
-
-function isLocalStack(resource: unknown): resource is LocalStack {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return (resource as any).constructor.name === "LocalStack";
 }
