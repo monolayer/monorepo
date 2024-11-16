61bbfda759345b662ba4644f7692e08265d78363
Marcos Essindi <marcessindi@me.com>
2024-11-16 13:48:00 +0100

    Export sc

--- packages/sidecar/src/index.ts
+++ packages/sidecar/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+import { PostgresDatabase } from "./resources/postgres-database.js";
+import { Redis } from "./resources/redis.js";
+
+export const sc = {
+	PostgresDatabase: PostgresDatabase,
+	Redis: Redis,
+};
