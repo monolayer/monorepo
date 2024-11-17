83f6325af76998f36a8de7aa48cfbf149ec3c498
Marcos Essindi <marcessindi@me.com>
2024-11-17 17:18:01 +0100

    Refine spinner message for PostgresDatabase

--- packages/sidecar/src/containers/admin/dev-container.ts
+++ packages/sidecar/src/containers/admin/dev-container.ts
@@ -2,6 +2,7 @@ import ora from "ora";
 import color from "picocolors";
 import { getExistingContainer } from "~sidecar/containers/admin/introspection.js";
 import { containerStarter } from "~sidecar/containers/container-starter.js";
+import type { PostgresDatabase } from "~sidecar/workloads.js";
 import { type Workload } from "~sidecar/workloads/stateful/interfaces.js";
 /**
  * Launches a dev container for a workload.
@@ -13,10 +14,7 @@ export async function startDevContainer(
 	workload: Workload,
 ) {
 	const spinner = ora();
-	spinner.start(
-		`Start ${workload.id} ${color.gray(workload.constructor.name)}`,
-	);
-
+	spinner.start(spinnerMessage(workload, "Start"));
 	const startedTestContainer = await containerStarter.startContainerForWorkload(
 		workload,
 		false,
@@ -39,7 +37,7 @@ export async function stopDevContainer(
 	workload: Workload,
 ) {
 	const spinner = ora();
-	spinner.start(`Stop ${workload.id} ${color.gray(workload.constructor.name)}`);
+	spinner.start(spinnerMessage(workload, "Stop"));
 	const startedTestContainer = await getExistingContainer(workload);
 
 	if (startedTestContainer === undefined) {
@@ -56,4 +54,15 @@ export async function stopDevContainer(
 		}
 	}
 	spinner.succeed();
+}
+function spinnerMessage(workload: Workload, prefix: "Start" | "Stop") {
+	let message = "";
+	if (workload.constructor.name === "PostgresDatabase") {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const databaseWorkload = workload as PostgresDatabase<any>;
+		message = `${prefix} ${databaseWorkload.databaseName} (${workload.id}) ${color.gray(workload.constructor.name)}`;
+	} else {
+		message = `${prefix} ${workload.id} ${color.gray(workload.constructor.name)}`;
+	}
+	return message;
 }
