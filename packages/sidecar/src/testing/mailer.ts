369f4e67c95fe648b04f29e76166a8d4b48f95ab
Marcos Essindi <marcessindi@me.com>
2024-11-18 23:12:57 +0100

    Use getExistingContainer

--- packages/sidecar/src/testing/mailer.ts
+++ packages/sidecar/src/testing/mailer.ts
@@ -1,30 +1,19 @@
-import { getContainerRuntimeClient } from "testcontainers";
-import { CONTAINER_LABEL_WORKLOAD_ID } from "~sidecar/containers/container.js";
+import { getExistingContainer } from "~sidecar/containers/admin/introspection.js";
 import type { Mailer } from "~sidecar/workloads/stateful/mailer.js";
 
 export async function testMailerURL<C>(mailer: Mailer<C>) {
 	const url = new URL(process.env[mailer.connectionStringEnvVar()]!);
 	const httpURL = new URL("", "http://base.com");
 	httpURL.host = url.host;
-	httpURL.port = await mailerWebUIHostPort(mailer.id);
+	httpURL.port = await mailerWebUIHostPort(mailer);
 	return httpURL.toString();
 }
 
-async function getMailerContainer(mailerId: string) {
-	const containerRuntimeClient = await getContainerRuntimeClient();
-	const container = await containerRuntimeClient.container.fetchByLabel(
-		CONTAINER_LABEL_WORKLOAD_ID,
-		`mailer-${mailerId}`,
-		{ status: ["running"] },
-	);
+async function mailerWebUIHostPort<C>(mailer: Mailer<C>) {
+	const container = await getExistingContainer(mailer);
 	if (container === undefined) {
-		throw new Error(`Container for mailer ${mailerId} not found`);
+		throw new Error(`container for Mailer workload ${mailer.id} not found`);
 	}
-	return container;
-}
-
-async function mailerWebUIHostPort(mailerId: string) {
-	const container = await getMailerContainer(mailerId);
 	const inspect = await container.inspect();
 	const ports = inspect.NetworkSettings.Ports;
 	return ports["8025/tcp"]![0]!["HostPort"];
