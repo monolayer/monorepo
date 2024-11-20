e3c584a367d37f551670471845668d57838ca4ee
Marcos Essindi <marcessindi@me.com>
2024-11-20 10:29:40 +0100

    Export types

--- packages/workloads/src/workloads.ts
+++ packages/workloads/src/workloads.ts
@@ -1,11 +1,14 @@
-export { StartOptions } from "~sidecar/containers/container.js";
+export type { StartOptions } from "~sidecar/containers/container.js";
 export { Bucket } from "~sidecar/workloads/stateful/bucket.js";
-export { DatabaseOptions } from "~sidecar/workloads/stateful/database.js";
+export type {
+	Database,
+	DatabaseOptions,
+} from "~sidecar/workloads/stateful/database.js";
 export { Mailer } from "~sidecar/workloads/stateful/mailer.js";
 export { MySqlDatabase } from "~sidecar/workloads/stateful/mysql-database.js";
 export { PostgresDatabase } from "~sidecar/workloads/stateful/postgres-database.js";
 export { Redis } from "~sidecar/workloads/stateful/redis.js";
-export {
+export type {
 	StatefulWorkload,
 	StatefulWorkloadWithClient,
 } from "~sidecar/workloads/stateful/stateful-workload.js";
