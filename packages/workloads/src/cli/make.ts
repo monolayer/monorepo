7e32279ab27ee8aba06c14a7bffb64baad32c270
Marcos Essindi <marcessindi@me.com>
2024-11-23 12:48:58 +0100

    Move schema to make

--- packages/workloads/src/cli/make.ts
+++ packages/workloads/src/cli/make.ts
@@ -1,7 +1,6 @@
 import { existsSync, mkdirSync, writeFileSync } from "node:fs";
 import path from "node:path";
 import { cwd } from "node:process";
-import { schema } from "~workloads/cli/make/schema.js";
 import {
 	assertElasticSearch,
 	assertMailer,
@@ -141,3 +140,98 @@ interface WorkloadInfo {
 	id: string;
 	connectionStringEnvVar: string;
 }
+
+export const schema = {
+	$schema: "https://json-schema.org/draft/2020-12/schema",
+	type: "object",
+	$id: "workloads-build-manifest-schema-v1",
+	title: "WorkloadsBuildManifest",
+	properties: {
+		version: {
+			type: "string",
+			enum: ["1"],
+			description:
+				"The version of the schema. This must be '1' for version 1 of the schema.",
+		},
+		postgresDatabase: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/DatabaseWorkloadInfo",
+			},
+		},
+		mysqlDatabase: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/DatabaseWorkloadInfo",
+			},
+		},
+		redis: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/WorkloadInfo",
+			},
+		},
+		elasticSearch: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/WorkloadInfo",
+			},
+		},
+		mongoDb: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/DatabaseWorkloadInfo",
+			},
+		},
+		mailer: {
+			type: "array",
+			items: {
+				$ref: "#/$defs/WorkloadInfo",
+				description: "Array of Mailer",
+			},
+		},
+	},
+	required: [
+		"postgresDatabase",
+		"mysqlDatabase",
+		"redis",
+		"elasticSearch",
+		"mongoDb",
+		"mailer",
+	],
+	$defs: {
+		DatabaseWorkloadInfo: {
+			type: "object",
+			properties: {
+				id: { type: "string" },
+				databases: {
+					type: "array",
+					items: {
+						$ref: "#/$defs/Database",
+					},
+				},
+			},
+			required: ["id", "databases"],
+		},
+		Database: {
+			type: "object",
+			properties: {
+				name: { type: "string" },
+				connectionStringEnvVar: { type: "string" },
+			},
+			required: ["name", "connectionStringEnvVar"],
+		},
+		WorkloadInfo: {
+			type: "object",
+			properties: {
+				id: {
+					type: "string",
+				},
+				connectionStringEnvVar: {
+					type: "string",
+				},
+			},
+			required: ["id", "connectionStringEnvVar"],
+		},
+	},
+};
