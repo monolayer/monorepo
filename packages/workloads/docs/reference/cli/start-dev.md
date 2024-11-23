ba3389c43004977be7565c6d08d8b29984bd942a
Marcos Essindi <marcessindi@me.com>
2024-11-23 12:55:03 +0100

    Remove --no-exit option

--- packages/workloads/docs/reference/cli/start-dev.md
+++ packages/workloads/docs/reference/cli/start-dev.md
@@ -10,12 +10,6 @@ Launches workloads for development.
 npx workloads start dev
 ```
 
-## Options
-
-| Option                   | Description                                  |
-| -------------------------| -------------------------------------------- |
-| `-n, --no-exit`          | Do not exit workloads' containers on close.  |
-
 ## Global Options
 
 | Option                   | Description                   |
--- packages/workloads/src/cli/actions/start.ts
+++ packages/workloads/src/cli/actions/start.ts
@@ -1,8 +1,5 @@
 import type { Command } from "@commander-js/extra-typings";
-import {
-	handleSigint,
-	startWorkloads,
-} from "~workloads/cli/start-workloads.js";
+import { startWorkloads } from "~workloads/cli/start-workloads.js";
 import { importWorkloads } from "~workloads/workloads/import.js";
 
 export function start(program: Command) {
@@ -18,16 +15,12 @@ function devStart(program: Command) {
 	return program
 		.command("dev")
 		.description("launch dev workloads")
-		.option("-n, --no-exit", "Do not exit workloads' containers on close")
-		.action(async (options) => {
+		.action(async () => {
 			const workloads = await importWorkloads();
 			startWorkloads(workloads, {
 				mode: "dev",
 				waitForHealthcheck: true,
 			});
-			if (options.exit) {
-				handleSigint(workloads);
-			}
 		});
 }
 
