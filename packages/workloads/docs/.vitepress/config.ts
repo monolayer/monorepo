b01e42289e7c205f66f9979f04fa3de2cb4d38dc
Marcos Essindi <marcessindi@me.com>
2024-11-25 14:45:19 +0100

    Add base

--- packages/workloads/docs/.vitepress/config.ts
+++ packages/workloads/docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
 	title: "Workloads",
+	base: "/workloads-docs",
 	description: "The backend sidecar framework",
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
