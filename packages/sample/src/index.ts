8478acf0df52784d90e8e5520044c9ff74e6f2ba
Marcos Essindi <marcessindi@me.com>
2024-12-07 08:59:24 +0100

    Add sample package

---
+++ packages/sample/src/index.ts
@@ -0,0 +1,15 @@
+export {
+	AddOptions,
+	BindMountType,
+	CacheMountType,
+	CopyOptions,
+	Dockerfile,
+	HealthCheckOptions,
+	MountType,
+	OnBuildOption,
+	SecretMountType,
+	SharedCopyAddOptions,
+	SshMountType,
+	TmpfsMountType,
+} from "@monorepo/docker/df.js";
+export { validate } from "@monorepo/docker/validator.js";
