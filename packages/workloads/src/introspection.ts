52a05f046a264729b7e6cee0f0af4c3da051c5fa
Marcos Essindi <marcessindi@me.com>
2024-12-22 17:23:51 +0100

    Add instrospection module

---
+++ packages/workloads/src/introspection.ts
@@ -0,0 +1,9 @@
+export {
+	manifestJsonSchema,
+	type BucketInfo,
+	type BuildManifest,
+	type CronInto,
+	type DatabaseWorkloadInfo,
+	type TaskInfo,
+	type WorkloadInfo,
+} from "~workloads/make/manifest.js";
