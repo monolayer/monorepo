c3933e930d83c643a875edf98fd5f92b599f530f
Marcos Essindi <marcessindi@me.com>
2025-04-13 15:25:25 +0200

    Output dockerfile

--- packages/nextjs-lambda-adapter/CHANGELOG.md
+++ packages/nextjs-lambda-adapter/CHANGELOG.md
@@ -6,6 +6,22 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.0-beta.1] - 2025-04-13
+
+### Changed
+
+- Adapter code destination folder.
+
+### Added
+
+- Output sample Dockerfile.
+
+## [1.0.0-beta.2] - 2025-03-31
+
+### Fixed
+
+- Missing return value in `AppResponse.write`.
+
 ## [1.0.0-beta.1] - 2025-03-31
 
 ### Added
--- packages/nextjs-lambda-adapter/docker/lambda-dockerfile.ts
+++ packages/nextjs-lambda-adapter/docker/lambda-dockerfile.ts
@@ -1,35 +1,6 @@
-#!/usr/bin/env node
-
 import { Dockerfile } from "@monolayer/dw";
-import { cpSync, mkdirSync } from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
-const adapterDir = path.join(process.cwd(), "nextjs-lambda-adapter");
-mkdirSync(adapterDir, { recursive: true });
-copyAdapter(adapterDir);
-generateDockerfile(adapterDir);
-console.log("Installed adapter in ./nextjs-lambda-adapter");
-process.exit(0);
-
-function copyAdapter(baseDir: string) {
-	const packageDir = path.join(
-		fileURLToPath(new URL(".", import.meta.url)),
-		"..",
-	);
-	const sourceDir = path.join(baseDir, "src");
-	mkdirSync(sourceDir, { recursive: true });
-	cpSync(path.join(packageDir, "adapter"), sourceDir, { recursive: true });
-}
-
-function generateDockerfile(baseDir: string) {
-	const dockerfile = nextJsDockerfile();
-	const dockerDir = path.join(baseDir, "docker");
-	mkdirSync(dockerDir, { recursive: true });
-	dockerfile.save(path.join(dockerDir, "lambda.Dockerfile"));
-}
-
-function nextJsDockerfile() {
+export function nextJsDockerfile() {
 	const df = new Dockerfile();
 	df.FROM("public.ecr.aws/lambda/nodejs:20", { as: "base" });
 	df.banner("Install dependencies");
@@ -60,6 +31,7 @@ function nextJsDockerfile() {
 	df.COPY(["."], "${LAMBDA_TASK_ROOT}/");
 	df.ENV("NEXT_PRIVATE_STANDALONE", "true");
 	df.RUN("npm run build");
+	df.RUN("npx --yes @monolayer/nextjs-lambda-adapter@latest");
 
 	df.banner("Copy app from build");
 	df.FROM("base", { as: "runner" });
@@ -71,9 +43,6 @@ function nextJsDockerfile() {
 		from: "builder",
 	});
 	df.COPY(["${LAMBDA_TASK_ROOT}/.next/static"], "./.next/static/", {
-		from: "builder",
-	});
-	df.COPY(["${LAMBDA_TASK_ROOT}/nextjs-lambda-adapter/src/"], "./adapter/", {
 		from: "builder",
 	});
 
