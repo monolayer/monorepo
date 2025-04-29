07b1d054660243822dee1258e5d6d969fd7545b8
Marcos Essindi <marcessindi@me.com>
2025-04-29 17:45:55 +0200

    Update task path

--- packages/task-sqs-adapter/src/handler/index.ts
+++ packages/task-sqs-adapter/src/handler/index.ts
@@ -41,7 +41,7 @@ export function makeSQSTaskHandler(opts: { tasksDir: string }) {
 
 				if (tasks[taskId] === undefined) {
 					tasks[taskId] = (
-						await import(path.join(opts.tasksDir, `${taskId}.cjs`))
+						await import(path.join(opts.tasksDir, taskId, "index.cjs"))
 					).default;
 				}
 
