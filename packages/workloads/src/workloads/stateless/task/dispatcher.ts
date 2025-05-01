573e1b458a4cc456494e17d0a524fa3a5e93999e
Marcos Essindi <marcessindi@me.com>
2025-05-01 08:36:01 +0200

    Refactor dispatcher

--- packages/workloads/src/workloads/stateless/task/dispatcher.ts
+++ packages/workloads/src/workloads/stateless/task/dispatcher.ts
@@ -8,18 +8,20 @@ export const dispatcherAdapterSymbol = Symbol.for(
 );
 
 export async function dispatcher(): Promise<typeof developmentDispatch> {
-	if (process.env.NODE_ENV !== "production") {
+	if (process.env.NODE_ENV === "production") {
+		const dispatcherLib = process.env.TASK_DISPATCHER;
+		if (dispatcherLib == undefined)
+			throw new Error(`undefined TASK_DISPATCHER`);
+
+		const dispatcher = (await import(dispatcherLib))
+			.dispatcher as typeof developmentDispatch;
+
+		if (dispatcher === undefined) throw new Error(`undefined dispatcher`);
+		return dispatcher;
+	} else {
 		if (process.env.NODE_ENV === "test") {
 			return testDispatch;
 		}
 		return developmentDispatch;
 	}
-	const dispatcherLib = process.env.TASK_DISPATCHER;
-	if (dispatcherLib == undefined) throw new Error(`undefined TASK_DISPATCHER`);
-
-	const dispatcher = (await import(dispatcherLib))
-		.dispatcher as typeof developmentDispatch;
-
-	if (dispatcher === undefined) throw new Error(`undefined dispatcher`);
-	return dispatcher;
 }
